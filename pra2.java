440423ae941a149f12259404057c2e1dc9ff3451
shuuma <s.kitamura.sys23@morijyobi.ac.jp>
2023-10-05 14:32:16 +0900

    pra2.javaの修正
    インデント追加

--- pra2.java
+++ pra2.java
@@ -1,8 +1,8 @@
 public class practice2 {
-public static void main(String[] args) {
-	for (int i = 0; i < 100; i++) {
-System.out.println(Hello World);
-	}
-}
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			System.out.println(Hello World);
+		}
+			}
 }
 
